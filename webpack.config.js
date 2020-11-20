e1f80745f874bc91e73a826c9527971eeed6ac12
s1ga <s.v.harlanov@gmail.com>
2020-11-21 01:07:57 +0300

    feat: create npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: "development",
-    entry: 'index.js',
+    entry: './index.js',
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
