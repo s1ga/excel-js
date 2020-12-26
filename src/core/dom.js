12a60b5e7488eb033567696edcd3cd0620f908ed
s1ga <s.v.harlanov@gmail.com>
2020-12-26 16:18:47 +0300

    fix: add return in Dom for chaining

--- src/core/dom.js
+++ src/core/dom.js
@@ -84,10 +84,12 @@ class Dom {
 
     addClass(className) {
         this.$el.classList.add(className)
+        return this
     }
 
     removeClass(className) {
         this.$el.classList.remove(className)
+        return this
     }
 
     css(styles = {}) {
