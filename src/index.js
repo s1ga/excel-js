4fde1df883ae7c9119bc2922efd3a53f8edafaf2
s1ga <s.v.harlanov@gmail.com>
2021-01-06 19:08:45 +0300

    feat: create Router

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import './scss/style.scss'
-import {Excel} from '@/components/excel/Excel'
-import {Header} from '@/components/header/Header'
-import {Toolbar} from '@/components/toolbar/Toolbar'
-import {Formula} from '@/components/formula/Formula'
-import {Table} from '@/components/table/Table'
-import {createStore} from '@core/createStore'
-import {rootReducer} from '@/store/rootReducer'
-import {storage, debounce} from '@core/utils'
-import {initialState} from '@/store/initialState'
+// import {Excel} from '@/components/excel/Excel'
+// import {Header} from '@/components/header/Header'
+// import {Toolbar} from '@/components/toolbar/Toolbar'
+// import {Formula} from '@/components/formula/Formula'
+// import {Table} from '@/components/table/Table'
+// import {createStore} from '@core/createStore'
+// import {rootReducer} from '@/store/rootReducer'
+// import {storage, debounce} from '@core/utils'
+// import {initialState} from '@/store/initialState'
+import {Router} from '@core/routes/Router'
 
-const store = createStore(rootReducer, initialState)
-const stateListener = debounce(state => {
-    storage('excel-state', state)
-}, 300)
+new Router('#app', {
 
-store.subscribe(stateListener)
-
-const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table],
-    store
 })
 
-excel.render()
+// const store = createStore(rootReducer, initialState)
+// const stateListener = debounce(state => {
+//     storage('excel-state', state)
+// }, 300)
+//
+// store.subscribe(stateListener)
+//
+// const excel = new Excel('#app', {
+//     components: [Header, Toolbar, Formula, Table],
+//     store
+// })
+//
+// excel.render()
